2553d06fec9b6dfd4953aa4ff98023893ac179d1
leojportes <leojportes@gmail.com>
2021-03-18 18:00:58 -0300

    :sparkles:Layout, buttons, images, Contraints, Autolayouts.

---
+++ Alura Viagens/Alura Viagens/Home/View/TableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  TableViewCell.swift
+//  Alura Viagens
+//
+//  Created by Leonardo Oliveira Portes on 18/03/21.
+//  Copyright © 2021 alura. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var labelTitulo: UILabel!
+    
+    @IBOutlet weak var labelQuantidadeDias: UILabel!
+    
+    @IBOutlet weak var labelpreco: UILabel!
+    
+    @IBOutlet weak var imagemViagem: UIImageView!
+    
+    
+}
